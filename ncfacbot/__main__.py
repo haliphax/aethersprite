2f9a2e2f7dc88c3fd403491c9ba06016f2a3fe1d
haliphax <haliphax@gmail.com>
2020-04-02 13:03:21 -0500

    initial commit

---
+++ ncfacbot/__main__.py
@@ -0,0 +1,22 @@
+# stdlib
+from os import environ
+# 3rd party
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+client.run(environ['DISCORD_TOKEN'])
