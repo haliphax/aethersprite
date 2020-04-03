da52e40fa32a4f42eaf745a15fd05b97e4f43a64
haliphax <haliphax@gmail.com>
2020-04-03 11:21:43 -0500

    more root level event logging

--- ncfacbot/__main__.py
+++ ncfacbot/__main__.py
@@ -2,9 +2,24 @@
 from . import bot, DISCORD_TOKEN, log
 from .commands import *
 
+
+@bot.event
+async def on_connect():
+    log.info('Connected to Discord')
+
+
+@bot.event
+async def on_disconnect():
+    log.info('Disconnected')
+
+
 @bot.event
 async def on_ready():
-    log.info(f'Logged in as {bot.user}')
+    log.info(f'Logged in as {bot.user} - ready!')
 
+
+@bot.event
+async def on_resumed():
+    log.info('Connection resumed')
 
 bot.run(DISCORD_TOKEN)
