c520ee137fb92fcfff32af6168e69d49df8d62d1
haliphax <haliphax@gmail.com>
2020-04-03 03:23:27 -0500

    optional args, check status

--- ncfacbot/commands/sm.py
+++ ncfacbot/commands/sm.py
@@ -3,7 +3,9 @@
 # stdlib
 import asyncio as aio
 from datetime import datetime as dt
+from math import ceil
 from time import time
+import typing
 # local
 from .. import bot, log
 # 3rd party
@@ -20,18 +22,21 @@ SM_LIMIT = 120
 countdowns = {}
 
 
-@bot.command()
-async def sm(ctx, n: int):
+@bot.command(brief='Start a Sorcerers Might countdown')
+async def sm(ctx, n: typing.Optional[int]):
     """
-    Start a Sorcerers Might countdown for <n> minutes, 0 to cancel
+    Start a Sorcerers Might countdown for n minutes
+    
+    You may also use a value of 0 to cancel the countdown. If no value is provided, the remaining time of the countdown will be shown.
 
-    Takes into account the current bug in Sorcerers Might where 5 minutes are
-    deducted erroneously with each game tick.
+    * Takes into account the current bug in Sorcerers Might where 5 minutes are deducted erroneously with each game tick.
     """
 
     if type(ctx.channel) is DMChannel:
         await ctx.send('This command must be used in a channel.')
         return
+
+    loop = aio.get_event_loop()
 
     # normalize nick
 
@@ -40,7 +45,23 @@ async def sm(ctx, n: int):
     if ctx.author.nick is not None:
         name = ctx.author.nick
 
-    # minute/minutes phrasing
+    if n is None:
+        # report countdown status
+        if name not in countdowns:
+            await ctx.send('You do not currently have a countdown.')
+            return
+
+        remaining = ceil((countdowns[name][0] - time()) / 60)
+        minutes = 'minutes' if remaining > 1 else 'minute'
+
+        if remaining < 1:
+            await ctx.send('Less than 1 minute remaining!')
+        else:
+            await ctx.send(f'{remaining} {minutes} to go.')
+
+        log.info(f'{ctx.author} checking SM status: {remaining} {minutes}')
+        return
+            
     minutes = 'minutes' if n > 1 else 'minute'
 
     if n > SM_LIMIT:
@@ -51,8 +72,8 @@ async def sm(ctx, n: int):
         return
 
     if name in countdowns:
-        # cancel existing callback, if any
-        countdowns[name].cancel()
+        # cancel callback, if any
+        countdowns[name][1].cancel()
 
         try:
             del countdowns[name]
@@ -60,7 +81,7 @@ async def sm(ctx, n: int):
             # overlapping requests due to lag can get out of sync
             pass
 
-        await ctx.send('Your existing countdown has been canceled.')
+        await ctx.send('Your countdown has been canceled.')
         log.info(f'{ctx.author} canceled SM countdown')
 
         # if no valid duration supplied, we're done
@@ -80,26 +101,28 @@ async def sm(ctx, n: int):
     next_tick = (now + FIFTEEN_MINS) - (now % FIFTEEN_MINS)
 
     if sm_end > next_tick:
+        dummy_sm = sm_end
+
         # walk through SM duration and subtract 5 mins for each AP tick
         while True:
-            diff = sm_end - next_tick
+            diff = dummy_sm - next_tick
 
             if diff < FIVE_MINS:
                 # less than 5 minute(s) left; shave off the difference and break
-                sm_end -= diff
+                dummy_sm -= diff
                 break
 
             next_tick = next_tick + FIFTEEN_MINS
-            sm_end -= FIVE_MINS
+            dummy_sm -= FIVE_MINS
 
-        reduced = max(1, int((sm_end - now) / 60))
+        reduced = max(1, int((dummy_sm - now) / 60))
 
         if reduced != n:
             output += f' (Adjusting to {reduced} due to SM bug.)'
             n = reduced
+            sm_end = now + (60 * n)
 
     output += '```'
-    loop = aio.get_event_loop()
 
     def done():
         "Countdown completed callback"
@@ -114,6 +137,6 @@ async def sm(ctx, n: int):
             del countdowns[name]
 
     # set timer for countdown completed callback
-    countdowns[name] = loop.call_later(60 * n, done)
+    countdowns[name] = (sm_end, loop.call_later(60 * n, done))
     await ctx.send(output)
     log.info(f'{ctx.author} started SM countdown for {n} {minutes}')
