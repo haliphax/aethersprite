ab2051c2f15fdd5b828025aebcd2621807209413
haliphax <haliphax@gmail.com>
2020-04-03 11:31:48 -0500

    tick command input limits

--- ncfacbot/commands/tick.py
+++ ncfacbot/commands/tick.py
@@ -13,6 +13,8 @@ from ..common import get_next_tick, normalize_username
 MINUTE = 60
 HOUR = MINUTE * 60
 DAY = HOUR * 24
+#: Future/past tick limit
+TICK_LIMIT = 1000
 
 
 @bot.command(brief='Next game tick')
@@ -20,14 +22,20 @@ async def tick(ctx, n: typing.Optional[int] = 1):
     """
     Next game tick
 
-    Show the next game tick in GMT. Provide a value for <n> to get the GMT timestamp of <n> ticks from now.
+    Show the next game tick in GMT. Provide a value for <n> to get the GMT timestamp of <n> ticks from now. For past ticks, use a negative number.
     """
+
+    if -TICK_LIMIT > n or n > TICK_LIMIT:
+        # let's not be silly, now
+        await ctx.message.add_reaction('\U0001F44E')
+        log.info(f'{ctx.author} made rejected next tick request of {n}')
+        return
 
     name = normalize_username(ctx.author)
     future_tick = get_next_tick() + timedelta(minutes=(n - 1) * 15)
     tick_str = future_tick.strftime('%a %Y-%m-%d %H:%M:%S %Z - ')
-    diff = (calendar.timegm(future_tick.timetuple())
-            - calendar.timegm(datetime.now(timezone.utc).timetuple()))
+    diff = abs(calendar.timegm(future_tick.timetuple())
+               - calendar.timegm(datetime.now(timezone.utc).timetuple()))
     until = ''
 
     if diff >= DAY:
@@ -45,7 +53,7 @@ async def tick(ctx, n: typing.Optional[int] = 1):
     if diff > 0:
         until += f'{diff} second(s) '
 
-    if not len(until):
+    if not len(until) and n > 0:
         until = 'right now!'
     else:
         until += 'from now'
