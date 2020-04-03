53d636fdc0af657b7e56b01a30e6062b9d619bbc
haliphax <haliphax@gmail.com>
2020-04-03 00:53:04 -0500

    timer -> countdown

--- ncfacbot/commands/sm.py
+++ ncfacbot/commands/sm.py
@@ -7,12 +7,12 @@ from .. import bot, log
 # 3rd party
 from discord import DMChannel
 
-timers = {}
+countdowns = {}
 FIVE_MINS = 60 * 5
 FIFTEEN_MINS = 60 * 15
 
 
-@bot.command(help='Start a Sorcerers Might timer for n minutes, 0 to cancel')
+@bot.command(help='Start a Sorcerers Might countdown for n minutes, 0 to cancel')
 async def sm(ctx, n: int):
     if type(ctx.channel) is DMChannel:
         await ctx.send('Sorry, but this command must be used in a channel.')
@@ -24,22 +24,22 @@ async def sm(ctx, n: int):
     if ctx.author.nick is not None:
         name = ctx.author.nick
 
-    if name in timers:
-        timers[name].cancel()
-        del timers[name]
-        await ctx.send('Your existing timer has been canceled.')
+    if name in countdowns:
+        countdowns[name].cancel()
+        del countdowns[name]
+        await ctx.send('Your existing countdown has been canceled.')
 
         if n < 1:
             return
 
     if n < 1:
-        await ctx.send('You do not currently have a timer.')
+        await ctx.send('You do not currently have a countdown.')
         return
 
     now = time()
     sm_end = now + (60 * n)
     next_tick = (now + FIFTEEN_MINS) - (now % FIFTEEN_MINS)
-    output = f'```{name} has started a Sorcerers Might timer for {n} minutes.'
+    output = f'```{name} has started a Sorcerers Might countdown for {n} minutes.'
 
     # adjust for SM bug
     if sm_end > next_tick:
@@ -70,6 +70,6 @@ async def sm(ctx, n: int):
             aio.gather(loop.create_task(ctx.send(
                 f'```Sorcerers Might countdown ended for {name}!```')))
         finally:
-            del timers[name]
+            del countdowns[name]
 
-    timers[name] = loop.call_later(60 * n, sm_complete)
+    countdowns[name] = loop.call_later(60 * n, sm_complete)
