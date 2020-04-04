b0f68cce9a67d1d1fa7190c41cdc082206208516
haliphax <haliphax@gmail.com>
2020-04-04 15:08:26 -0500

    __main__ check before bot.run

--- ncfacbot/__main__.py
+++ ncfacbot/__main__.py
@@ -6,9 +6,6 @@ from sys import stdout
 # local
 from . import bot, log
 
-# need credentials
-assert 'DISCORD_TOKEN' in environ, \
-    'DISCORD_TOKEN not found in environment variables'
 # logging stuff
 streamHandler = logging.StreamHandler(stdout)
 streamHandler.setFormatter(logging.Formatter(
@@ -40,5 +37,16 @@ async def on_resumed():
 
 # load our commands semi-dynamically after everything's set up
 from .commands import *
-# here we go!
-bot.run(environ['DISCORD_TOKEN'])
+
+
+# redundant, but one last check in case someone wants to get real shifty and
+# do something that requires them to import __main__ from another entry point
+def entrypoint():
+    # need credentials
+    assert 'DISCORD_TOKEN' in environ, \
+        'DISCORD_TOKEN not found in environment variables'
+    # here we go!
+    bot.run(environ['DISCORD_TOKEN'])
+
+if __name__ == '__main__':
+    entrypoint()
