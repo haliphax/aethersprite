a6eafb17c709f64605da8674753c5548e2c10cfd
haliphax <haliphax@gmail.com>
2020-08-14 12:59:21 -0500

    use env vars for host/port of web app

--- ncfacbot/webapp/__main__.py
+++ ncfacbot/webapp/__main__.py
@@ -1,7 +1,10 @@
 "Web application entry point"
 
+# stdlib
+from os import environ
 # local
 from . import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host=environ.get('NCFACBOT_HOST', '0.0.0.0'),
+            port=int(environ.get('NCFACBOT_PORT', 5000)))
