108bc4f15752bb007fb6f3f02c717a550edd1f14
haliphax <haliphax@gmail.com>
2020-08-17 12:29:14 -0500

    document Flask app var

--- ncfacbot/webapp/__init__.py
+++ ncfacbot/webapp/__init__.py
@@ -10,6 +10,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 # local
 from .. import extensions
 
+#: Flask application
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, environ.get('PROXIES', 1))
 _mods = [m for m in dir(extensions) if m[0] != '_']
