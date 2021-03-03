7581663976c4cf283798dfdd2a661f8e145e8db1
haliphax <haliphax@gmail.com>
2021-03-03 01:01:35 -0600

    multiple param for filters, fixups

--- aethersprite/filters.py
+++ aethersprite/filters.py
@@ -3,6 +3,7 @@
 # stdlib
 import re
 # local
+from . import log
 from .common import (get_channel_for_id, get_id_for_channel, get_id_for_role,
                      get_role_for_id)
 from .settings import SettingFilter, settings
@@ -12,50 +13,34 @@ class ChannelFilter(SettingFilter):
 
     "Filter used for converting channel names to IDs and back"
 
-    def in_(self, ctx, value: str):
-        match = re.match(r'^<#(\d+)>$', value.strip())
-        val = None
+    #: True to allow multiple values
+    multiple: bool = True
 
-        if match is None:
-            val = get_id_for_channel(ctx.guild, value)
-        else:
-            val = int(match.groups()[0])
-
-        if val is None:
-            raise ValueError()
-
-        return val
-
-    def out(self, ctx) -> str:
-        val = settings[self.setting].get(ctx, raw=True)
-        val = get_channel_for_id(ctx.guild, val)
-
-        return val
-
-ChannelFilter.in_.__doc__ = SettingFilter.in_.__doc__
-ChannelFilter.out.__doc__ = SettingFilter.out.__doc__
-
-
-class RoleFilter(SettingFilter):
-
-    "Filter used for converting role names to IDs and back"
+    def __init__(self, setting: str, multiple: bool = False):
+        super().__init__(setting)
+        self.multiple = multiple
 
     def in_(self, ctx, value: str):
-        roles = [v.strip() for v in value.split(',')]
+        channels = re.findall(r'<#(\d+)> ?|([-_a-zA-Z0-9]+)[, ]*', value.strip())
+        log.info(channels)
         ids = []
 
-        for r in roles:
-            match = re.match(r'^<@&(\d+)>$', r)
+        for c in channels:
+            id = None
 
-            if match is None:
-                id = get_id_for_role(ctx.guild, r)
+            if c[0] == '':
+                id = get_id_for_channel(ctx.guild, c[1])
             else:
-                id = int(match.groups()[0])
+                # convert mentions, if any
+                id = int(c[0])
 
             if id is None:
-                raise ValueError(r)
+                raise ValueError()
 
             ids.append(id)
+
+            if not self.multiple:
+                break
 
         return ids
 
@@ -65,7 +50,53 @@ class RoleFilter(SettingFilter):
         if value is None:
             return
 
-        return ', '.join([get_role_for_id(ctx.guild, v) for v in value])
+        return [get_channel_for_id(ctx.guild, v) for v in value]
+
+ChannelFilter.in_.__doc__ = SettingFilter.in_.__doc__
+ChannelFilter.out.__doc__ = SettingFilter.out.__doc__
+
+
+class RoleFilter(SettingFilter):
+
+    "Filter used for converting role names to IDs and back"
+
+    #: True to allow multiple values
+    multiple: bool = True
+
+    def __init__(self, setting: str, multiple: bool = True):
+        super().__init__(setting)
+        self.multiple = multiple
+
+    def in_(self, ctx, value: str):
+        roles = re.findall(r'<@&(\d+)> ?|([^,]+)[, ]*', value)
+        ids = []
+
+        for r in roles:
+            id = None
+
+            if r[0] == '':
+                id = get_id_for_role(ctx.guild, r[1])
+            else:
+                # convert mentions, if any
+                id = int(r[0])
+
+            if id is None:
+                raise ValueError(r)
+
+            ids.append(id)
+
+            if not self.multiple:
+                break
+
+        return ids
+
+    def out(self, ctx) -> str:
+        value = settings[self.setting].get(ctx, raw=True)
+
+        if value is None:
+            return
+
+        return [get_role_for_id(ctx.guild, v) for v in value]
 
 RoleFilter.in_.__doc__ = SettingFilter.in_.__doc__
 RoleFilter.out.__doc__ = SettingFilter.out.__doc__
